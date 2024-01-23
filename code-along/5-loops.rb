cea8bc3e6f643ecf3bb7a755cad717fc95d6afc0
Hans Han <hans.han@kellogg.northwestern.edu>
2024-01-23 17:30:11 +0000

    Class Final Loops

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -7,5 +7,32 @@
 #   puts "tacos!"
 # end
 
-# Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+#Loop through tacos
+tacos = ["carnitas", "carne asada", "pollo", "pescado", "veggie"]
+
+# puts "Eat #{tacos[0]}"
+
+# start loop
+#loop do
+
+    # # break the loop if the number of elements is the same as the index
+    # if tacos.length == index
+    #     break
+    # end
+    # #assign a variable to each taco
+    # taco = tacos[index]
+
+    # #print the output
+    # puts "Eat #{tacos [index]} tacos!"
+
+    # #increment the index
+    # index = index + 1
+
+   
+
+#end
+for taco in tacos
+    puts "Eat #{taco} tacos!"
+end
+
+
